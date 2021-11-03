a6a5e2fc69fcbf01a1d6be6b4b99c92b74ba0e81
Carson <59584865+Carson365@users.noreply.github.com>
2021-11-03 15:29:37 -0500

    Introduce Math

--- Forms/frm10VideoGames.cs
+++ Forms/frm10VideoGames.cs
@@ -1,3 +1,4 @@
+
 //
 // SOURCES:
 //
@@ -26,6 +27,11 @@ namespace Mathre
 			Load += FormLoad;
 			btnPS4.Click += ImageSetter;
 			btnXB1.Click += ImageSetter;
+			btnPS4.Click += Cost;
+			btnXB1.Click += Cost;
+			txtDoom.TextChanged += Cost;
+			txtStardew.TextChanged += Cost;
+			txtMinecraft.TextChanged += Cost;
 		}
 		public void FormLoad(object sender, EventArgs e)
 		{
@@ -37,6 +43,7 @@ namespace Mathre
 			}
 			ImageSetter(null, null);
 		}
+
 		public void ImageSetter(object sender, EventArgs e)
 		{
 			
@@ -51,6 +58,48 @@ namespace Mathre
 				pnlDoom.BackgroundImage = imgGames.Images["Doom-XB1.jpg"];
 				pnlMinecraft.BackgroundImage = imgGames.Images["Minecraft-XB1.jpg"];
 				pnlStardew.BackgroundImage = imgGames.Images["Stardew-XB1.jpg"];
+			}
+		}
+		public void Cost(object sender, EventArgs e)
+		{
+			int Doom = 0;
+			if (int.TryParse(txtDoom.Text, out int DoomCount))
+			{
+				Doom = DoomCount;
+			}
+			int Stardew = 0;
+			if (int.TryParse(txtStardew.Text, out int StardewCount))
+			{
+				Stardew = StardewCount;
+			}
+			int Minecraft = 0;
+			if (int.TryParse(txtMinecraft.Text, out int MinecraftCount))
+			{
+				Minecraft = MinecraftCount;
+			}
+			if (txtDoom.Text != "" | txtStardew.Text != "" | txtMinecraft.Text != "")
+			{
+				int A = (Doom+Minecraft+Stardew);
+				string B = A switch { not 1 => "s", _ => "" };
+				int C = (A / 3);
+				string D = C switch { not 1 => "es", _ => "" };
+				double E = (A % 3);
+				string F = (A % 3) switch { not 1 => "s", _ => "" };
+				lblCostText.Text = $"Order {A} Game{B} :  $\n Tax @ 6.75% :  $\n {C} box{D} :  $\n {E} Envelope{F} :  $\n Total :  $";
+				double A2 = Convert.ToDouble(A) * 59.99;
+				double B2 = A2 * 0.0675;
+				double C2 = C;
+				double D2 = E * 0.5;
+				double E2 = A2 + B2 + C2 + D2;
+				string A3 = (A2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
+				string B3 = (B2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
+				string D3 = (D2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
+				string E3 = (E2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
+				lblCost.Text = $" {Math.Round(A2,2)}{A3}\n {Math.Round(B2, 2)}{B3}\n {Math.Round(C2, 2)}.00\n {Math.Round(D2, 2)}{D3}\n {Math.Round(E2, 2)}{E3}";
+			}
+			else
+			{
+				lblCostText.Text = "";
 			}
 		}
 	}
