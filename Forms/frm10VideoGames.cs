ab4a9f8d8dbdd29619be52d744f2dc5902cb897b
Carson <59584865+Carson365@users.noreply.github.com>
2021-11-04 14:54:09 -0500

    Complete VideoGame implementation

--- Forms/frm10VideoGames.cs
+++ Forms/frm10VideoGames.cs
@@ -1,18 +1,11 @@
-
-//
 // SOURCES:
-//
 // Stardew / PS4 : https://m.media-amazon.com/images/I/81VgMy0mHYL._SL100_.jpg
 // Stardew / XB1 : https://m.media-amazon.com/images/I/81ELSb3bhGL._SL100_.jpg
 // Minecraft / PS4 : https://m.media-amazon.com/images/I/71AKO+U6F6L._SL100_.jpg
 // Minecraft / XB1 : https://m.media-amazon.com/images/I/71QpSBZtkqL._SL100_.jpg
 // Doom / PS4 : https://m.media-amazon.com/images/I/91uV7r7aAhL._SL100_.jpg
 // Doom / XB1 : https://m.media-amazon.com/images/I/61bF5-QAX+L._SL100_.jpg
-//
-//
-//
 using System;
-using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 namespace Mathre
@@ -27,11 +20,12 @@ namespace Mathre
 			Load += FormLoad;
 			btnPS4.Click += ImageSetter;
 			btnXB1.Click += ImageSetter;
-			btnPS4.Click += Cost;
-			btnXB1.Click += Cost;
 			txtDoom.TextChanged += Cost;
 			txtStardew.TextChanged += Cost;
 			txtMinecraft.TextChanged += Cost;
+			txtDoom.KeyPress += InputFormatter;
+			txtStardew.KeyPress += InputFormatter;
+			txtMinecraft.KeyPress += InputFormatter;
 		}
 		public void FormLoad(object sender, EventArgs e)
 		{
@@ -42,8 +36,21 @@ namespace Mathre
 				BaseForm.GetAllControls(c);
 			}
 			ImageSetter(null, null);
+			lblCost.Text = "";
+			lblCostText.Text = "";
 		}
-
+		public void Transfer(object sender, EventArgs e)
+		{
+			if (ReferenceEquals(sender, BaseForm.mnuXB1))
+			{
+				ThisForm.btnXB1.Checked = true;
+			}
+			else if (ReferenceEquals(sender, BaseForm.mnuPS4))
+			{
+				ThisForm.btnPS4.Checked = true;
+			}
+			ThisForm.ImageSetter(null, null);
+		}
 		public void ImageSetter(object sender, EventArgs e)
 		{
 			
@@ -62,21 +69,9 @@ namespace Mathre
 		}
 		public void Cost(object sender, EventArgs e)
 		{
-			int Doom = 0;
-			if (int.TryParse(txtDoom.Text, out int DoomCount))
-			{
-				Doom = DoomCount;
-			}
-			int Stardew = 0;
-			if (int.TryParse(txtStardew.Text, out int StardewCount))
-			{
-				Stardew = StardewCount;
-			}
-			int Minecraft = 0;
-			if (int.TryParse(txtMinecraft.Text, out int MinecraftCount))
-			{
-				Minecraft = MinecraftCount;
-			}
+			int Doom = int.Parse(txtDoom.Text);
+			int Stardew = int.Parse(txtStardew.Text);
+			int Minecraft = int.Parse(txtMinecraft.Text);
 			if (txtDoom.Text != "" | txtStardew.Text != "" | txtMinecraft.Text != "")
 			{
 				int A = (Doom+Minecraft+Stardew);
@@ -85,21 +80,35 @@ namespace Mathre
 				string D = C switch { not 1 => "es", _ => "" };
 				double E = (A % 3);
 				string F = (A % 3) switch { not 1 => "s", _ => "" };
-				lblCostText.Text = $"Order {A} Game{B} :  $\n Tax @ 6.75% :  $\n {C} box{D} :  $\n {E} Envelope{F} :  $\n Total :  $";
-				double A2 = Convert.ToDouble(A) * 59.99;
+				lblCostText.Text = $"Order {A} Game{B} :\n Tax @ 6.75% :\n {C} box{D} :\n {E} Envelope{F} :\n Total :";
+				double A2 = (Convert.ToDouble(A) * 59.99);
 				double B2 = A2 * 0.0675;
 				double C2 = C;
 				double D2 = E * 0.5;
 				double E2 = A2 + B2 + C2 + D2;
-				string A3 = (A2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
-				string B3 = (B2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
-				string D3 = (D2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
-				string E3 = (E2.ToString().Split('.'))[1].Length switch { 0 => "00", 1 => "0", _ => "" };
-				lblCost.Text = $" {Math.Round(A2,2)}{A3}\n {Math.Round(B2, 2)}{B3}\n {Math.Round(C2, 2)}.00\n {Math.Round(D2, 2)}{D3}\n {Math.Round(E2, 2)}{E3}";
+				lblCost.Text = $" {A2:C}\n {B2:C}\n {C2:C}\n {D2:C}\n {E2:C}";
 			}
 			else
 			{
 				lblCostText.Text = "";
+				lblCost.Text = "";
+			}
+		}
+		public void InputFormatter(object sender, KeyPressEventArgs e)
+		{
+			if (sender is not TextBoxBase textBox)
+				return;
+			if ((e.KeyChar != '\b' && (e.KeyChar < '0' || e.KeyChar > '9')))
+			{
+				e.Handled = true;
+			}
+			if (textBox.Text.Length == 3 && e.KeyChar != '\b')
+			{
+						e.Handled = true;
+			}
+			if (textBox.Text.Length == 0 && e.KeyChar == '0')
+			{
+				e.Handled = true;
 			}
 		}
 	}
