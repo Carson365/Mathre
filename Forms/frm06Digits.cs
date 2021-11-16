8e3ef30618245b054ed8bdce858879f6919da8bd
Carson365 <carsonmathre@gmail.com>
2021-11-15 22:43:53 -0600

    Work on Digits logic

--- Forms/frm06Digits.cs
+++ Forms/frm06Digits.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -8,13 +8,12 @@ namespace Mathre
 	public partial class FrmDigits : Form
 	{
 		public static FrmMathre BaseForm;
-		public Dictionary<int, char> newDictionary = new();
+		public OrderedDictionary betterDictionary = new();
 		public FrmDigits()
 		{
 			InitializeComponent();
 			Load += FormLoad;
-			//txtNumber.KeyUp += Digits;
-			txtNumber.TextChanged += New;
+			txtNumber.TextChanged += Digits;
 			txtNumber.KeyPress += InputFormatter;
 		}
 		public void FormLoad(object sender, EventArgs e)
@@ -27,37 +26,44 @@ namespace Mathre
 				BaseForm.GetAllControls(c);
 			}
 		}
-		public void Digits(object sender, KeyEventArgs e)
+		public void Digits(object sender, EventArgs e)
 		{
-			lblDigitsCount.Text = txtNumber.Text.Length.ToString();
+			int.TryParse(lblDigitsCount.Text, out int a);
+			if (txtNumber.Text.Length > a)
+			{
+				var label = (txtNumber.SelectionStart % 2) switch { 0 => lblDigitsListEvens, _ => lblDigitsListOdds };
+				if (label.Text.Contains($"Digit {txtNumber.SelectionStart}:"))
+				{
+					DigitsRedo(null, null);
+				}
+				else
+				{
+					label.Text += $"Digit {txtNumber.SelectionStart}: {txtNumber.Text[txtNumber.SelectionStart - 1]}\r\n";
+				}
+			}
+			else
+			{
+				if (txtNumber.SelectionStart == txtNumber.Text.Length)
+				{
+					var label = (txtNumber.SelectionStart % 2) switch { 1 => lblDigitsListEvens, _ => lblDigitsListOdds };
+					label.Text = label.Text.Remove(label.Text.Length - 13);
+				}
+				else
+				{
+					DigitsRedo(null, null);
+				}
+			}
+			lblDigitsCount.Text = $"{txtNumber.Text.Length}";
+		}
+		public void DigitsRedo(object sender, KeyEventArgs e)
+		{
 			lblDigitsListEvens.Text = "";
 			lblDigitsListOdds.Text = "";
 			for (int i = 0; i < txtNumber.Text.Length; i++)
 			{
 				char c = txtNumber.Text[i];
-				if ((txtNumber.Text.IndexOf(c, i) + 1) % 2 == 0)
-				{
-					lblDigitsListEvens.Text += $"Digit {i + 1}: {c}\r\n";
-				}
-				else if ((txtNumber.Text.IndexOf(c, i) + 1) % 2 == 1)
-				{
-					lblDigitsListOdds.Text += $"Digit {i + 1}: {c}\r\n";
-				}
-			}
-		}
-		public void New(object sender, EventArgs e)
-		{
-			lblDigitsCount.Text = txtNumber.Text.Length.ToString();
-			if (txtNumber.Text.Length > newDictionary.Keys.Count)
-			{
-				newDictionary.Add(txtNumber.Text.Length, txtNumber.Text[txtNumber.Text.Length - 1]);
-				lblDigitsListEvens.Text += $"Digit {txtNumber.Text.Length}: {newDictionary[txtNumber.Text.Length]}\r\n";
-			}
-			else if (txtNumber.Text.Length <= newDictionary.Keys.Count)
-			{
-				Console.WriteLine(lblDigitsListEvens.Text.Length - 12);
-				newDictionary.Remove(txtNumber.Text.Length + 1);
-				lblDigitsListEvens.Text = lblDigitsListEvens.Text.Remove(lblDigitsListEvens.Text.Length - 12, 12);
+				var label = ((txtNumber.Text.IndexOf(c, i) + 1) % 2) switch { 0 => lblDigitsListEvens, _ => lblDigitsListOdds };
+				label.Text += $"Digit {i + 1}: {c}\r\n";
 			}
 		}
 		public void InputFormatter(object sender, KeyPressEventArgs e)
