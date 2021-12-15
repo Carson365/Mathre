702614dac638698627ca7c201898b941022fd07f
Carson Mathre <carsonmathre@gmail.com>
2021-12-15 12:56:03 -0600

    Acronym Work Start

--- Forms/frm14Acronym.Designer.cs
+++ Forms/frm14Acronym.Designer.cs
@@ -28,98 +28,72 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.pnlSecretFrame = new System.Windows.Forms.Panel();
-            this.pnlSecret = new System.Windows.Forms.Panel();
-            this.lblSecretPrompt = new System.Windows.Forms.Label();
-            this.txtSecretPassword = new System.Windows.Forms.TextBox();
-            this.lblSecretTitle = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pnlSecretFrame.SuspendLayout();
-            this.pnlSecret.SuspendLayout();
+            this.pnlFrame = new System.Windows.Forms.Panel();
+            this.pnlBody = new System.Windows.Forms.Panel();
+            this.lblAcronym = new System.Windows.Forms.Label();
+            this.lblAcronymPrompt = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlFrame.SuspendLayout();
+            this.pnlBody.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pnlSecretFrame
+            // pnlFrame
             // 
-            this.pnlSecretFrame.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pnlFrame.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlSecretFrame.BackColor = System.Drawing.Color.Transparent;
-            this.pnlSecretFrame.Controls.Add(this.pnlSecret);
-            this.pnlSecretFrame.Controls.Add(this.lblSecretTitle);
-            this.pnlSecretFrame.Location = new System.Drawing.Point(-8, -20);
-            this.pnlSecretFrame.Name = "pnlSecretFrame";
-            this.pnlSecretFrame.Size = new System.Drawing.Size(1000, 500);
-            this.pnlSecretFrame.TabIndex = 33;
+            this.pnlFrame.BackColor = System.Drawing.Color.Transparent;
+            this.pnlFrame.Controls.Add(this.pnlBody);
+            this.pnlFrame.Controls.Add(this.lblTitle);
+            this.pnlFrame.Location = new System.Drawing.Point(-8, -20);
+            this.pnlFrame.Name = "pnlFrame";
+            this.pnlFrame.Size = new System.Drawing.Size(1000, 500);
+            this.pnlFrame.TabIndex = 33;
             // 
-            // pnlSecret
+            // pnlBody
             // 
-            this.pnlSecret.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pnlSecret.BackColor = System.Drawing.SystemColors.Control;
-            this.pnlSecret.Controls.Add(this.label1);
-            this.pnlSecret.Controls.Add(this.textBox1);
-            this.pnlSecret.Controls.Add(this.lblSecretPrompt);
-            this.pnlSecret.Controls.Add(this.txtSecretPassword);
-            this.pnlSecret.Location = new System.Drawing.Point(287, 165);
-            this.pnlSecret.Name = "pnlSecret";
-            this.pnlSecret.Size = new System.Drawing.Size(426, 105);
-            this.pnlSecret.TabIndex = 32;
+            this.pnlBody.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlBody.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlBody.Controls.Add(this.lblAcronym);
+            this.pnlBody.Controls.Add(this.lblAcronymPrompt);
+            this.pnlBody.Location = new System.Drawing.Point(287, 165);
+            this.pnlBody.Name = "pnlBody";
+            this.pnlBody.Size = new System.Drawing.Size(426, 105);
+            this.pnlBody.TabIndex = 32;
             // 
-            // lblSecretPrompt
+            // lblAcronym
             // 
-            this.lblSecretPrompt.AutoSize = true;
-            this.lblSecretPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSecretPrompt.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblSecretPrompt.Location = new System.Drawing.Point(28, 20);
-            this.lblSecretPrompt.Name = "lblSecretPrompt";
-            this.lblSecretPrompt.Size = new System.Drawing.Size(129, 20);
-            this.lblSecretPrompt.TabIndex = 9;
-            this.lblSecretPrompt.Text = "Starting Number:";
+            this.lblAcronym.AutoSize = true;
+            this.lblAcronym.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcronym.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblAcronym.Location = new System.Drawing.Point(163, 20);
+            this.lblAcronym.Name = "lblAcronym";
+            this.lblAcronym.Size = new System.Drawing.Size(71, 20);
+            this.lblAcronym.TabIndex = 10;
+            this.lblAcronym.Text = "Acronym";
             // 
-            // txtSecretPassword
+            // lblAcronymPrompt
             // 
-            this.txtSecretPassword.BackColor = System.Drawing.SystemColors.Window;
-            this.txtSecretPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtSecretPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSecretPassword.Location = new System.Drawing.Point(163, 18);
-            this.txtSecretPassword.Name = "txtSecretPassword";
-            this.txtSecretPassword.PasswordChar = '∗';
-            this.txtSecretPassword.Size = new System.Drawing.Size(114, 26);
-            this.txtSecretPassword.TabIndex = 9;
+            this.lblAcronymPrompt.AutoSize = true;
+            this.lblAcronymPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcronymPrompt.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblAcronymPrompt.Location = new System.Drawing.Point(28, 20);
+            this.lblAcronymPrompt.Name = "lblAcronymPrompt";
+            this.lblAcronymPrompt.Size = new System.Drawing.Size(75, 20);
+            this.lblAcronymPrompt.TabIndex = 9;
+            this.lblAcronymPrompt.Text = "Acronym:";
             // 
-            // lblSecretTitle
+            // lblTitle
             // 
-            this.lblSecretTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.lblSecretTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSecretTitle.Location = new System.Drawing.Point(187, 95);
-            this.lblSecretTitle.Margin = new System.Windows.Forms.Padding(0);
-            this.lblSecretTitle.Name = "lblSecretTitle";
-            this.lblSecretTitle.Size = new System.Drawing.Size(626, 51);
-            this.lblSecretTitle.TabIndex = 31;
-            this.lblSecretTitle.Text = "Enable Super Secret Settings";
-            this.lblSecretTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(34, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 20);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Ending Number:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(163, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '∗';
-            this.textBox1.Size = new System.Drawing.Size(114, 26);
-            this.textBox1.TabIndex = 11;
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(187, 95);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(626, 51);
+            this.lblTitle.TabIndex = 31;
+            this.lblTitle.Text = "Acronym Maker";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmAcronym
             // 
@@ -127,24 +101,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(984, 461);
-            this.Controls.Add(this.pnlSecretFrame);
+            this.Controls.Add(this.pnlFrame);
             this.Name = "FrmAcronym";
             this.Text = "Acronym Maker";
-            this.pnlSecretFrame.ResumeLayout(false);
-            this.pnlSecret.ResumeLayout(false);
-            this.pnlSecret.PerformLayout();
+            this.pnlFrame.ResumeLayout(false);
+            this.pnlBody.ResumeLayout(false);
+            this.pnlBody.PerformLayout();
             this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		public System.Windows.Forms.Panel pnlSecretFrame;
-		private System.Windows.Forms.Panel pnlSecret;
-		internal System.Windows.Forms.Label lblSecretPrompt;
-		internal System.Windows.Forms.TextBox txtSecretPassword;
-		internal System.Windows.Forms.Label lblSecretTitle;
-		internal System.Windows.Forms.Label label1;
-		internal System.Windows.Forms.TextBox textBox1;
+		public System.Windows.Forms.Panel pnlFrame;
+		private System.Windows.Forms.Panel pnlBody;
+		internal System.Windows.Forms.Label lblAcronymPrompt;
+		internal System.Windows.Forms.Label lblTitle;
+		internal System.Windows.Forms.Label lblAcronym;
 	}
 }
