edb0e104f3e11c88d95f235602e4cf663d91c10c
Carson Mathre <carsonmathre@gmail.com>
2022-02-23 14:14:35 -0600

    Add Shell Game code comments

--- Forms/frm20Shell.cs
+++ Forms/frm20Shell.cs
@@ -10,22 +10,22 @@ namespace Mathre
 			picShell1.Click += Default;
 			picShell2.Click += Default;
 			picShell3.Click += Default;
-			foreach (Control b in pnlBody.Controls) if (b.GetType() == typeof(PictureBox) && b.Name.Contains("Pearl")) b.Visible = false;
+			foreach (Control b in pnlBody.Controls) if (b.GetType() == typeof(PictureBox) && b.Name.Contains("Pearl")) b.Visible = false; // Hide all of the pearls
 		}
 		public void Default(object sender, EventArgs e)
 		{
-			string wonlost = "lost";
+			string wonlost = "lost"; // Default to lost
 			Random rnd = new();
 			int rnum = rnd.Next(1, 4);
 			PictureBox pb = sender as PictureBox;
-			if (pb.Name[8] == $"{rnum}"[0])
+			if (pb.Name[8] == $"{rnum}"[0]) // If the clicked picturebox is the randomly chosen one
 			{
-				foreach (Control b in pnlBody.Controls) if (b.GetType() == typeof(PictureBox) && b.Name.Contains("Pearl") && b.Name[8] == $"{rnum}"[0])
-					{ b.Visible = true; wonlost = "won"; lblWins.Text = $"{Convert.ToInt32(lblWins.Text) + 1}"; }
+				foreach (Control b in pnlBody.Controls) if (b.GetType() == typeof(PictureBox) && b.Name.Contains($"Pearl{Convert.ToString(rnum)[0]}")) // Get the right pearl
+					{ b.Visible = true; wonlost = "won"; lblWins.Text = $"{Convert.ToInt32(lblWins.Text) + 1}"; } // Set winstate, increment wins and show the pearl
 			}
-			MessageBox.Show($"You have {wonlost}.", "Results");
-			foreach (Control b in pnlBody.Controls) if (b.GetType() == typeof(PictureBox) && b.Name.Contains("Pearl")) b.Visible = false;
-			lblGames.Text = $"{Convert.ToInt32(lblGames.Text) + 1}";
+			MessageBox.Show($"You have {wonlost}.", "Results"); // Display the won message, then...
+			foreach (Control b in pnlBody.Controls) if (b.GetType() == typeof(PictureBox) && b.Name.Contains("Pearl")) b.Visible = false; // Re-hide all of the pearls
+			lblGames.Text = $"{Convert.ToInt32(lblGames.Text) + 1}"; // Increment total games
 		}
 	}
 }
