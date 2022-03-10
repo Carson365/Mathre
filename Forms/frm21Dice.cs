50ac4402e510d869c6d8f942639d4f5c8229a969
Carson Mathre <carsonmathre@gmail.com>
2022-03-10 14:54:26 -0600

    Implement Numericupdown fibonacci sequence

--- Forms/frm21Dice.cs
+++ Forms/frm21Dice.cs
@@ -4,38 +4,38 @@ namespace Mathre
 {
 	public partial class Frm21Dice : Form
 	{
-		decimal wagerstep = 0;
-		decimal lastvalue = 0;
-		decimal wagersave = 0;
 		public Frm21Dice()
 		{
 			InitializeComponent();
-			//numWager.Click += Wage;
-			numWager.ValueChanged += Wage;
+			Shown += Default;
+		}
+		class MyNUD : NumericUpDown
+		{
+			public override void UpButton() { int n = (int)Math.Round((double)Value * ((1 + Math.Sqrt(5)) / 2.0)); if (n < Maximum) Value = n; }
+			public override void DownButton() { int n = (int)Math.Round((double)Value / ((1 + Math.Sqrt(5)) / 2.0)); if (n >= Minimum) Value = n; }
 		}
 		public void Default(object sender, EventArgs e)
 		{
-
+			MyNUD numer = new();
+			numer.Size = numWager.Size;
+			numer.Location = numWager.Location;
+			numer.Value = 1;
+			numer.Minimum = 1;
+			numer.Maximum = 1000;
+			numer.KeyPress += InputFormatter;
+			pnlWager.Controls.Add(numer);
+			numWager.Hide();
+			numer.Show();
+		}
+		public void InputFormatter(object sender, KeyPressEventArgs e)
+		{
+			NumericUpDown box = sender as NumericUpDown;
+			if (e.KeyChar != '\b' && e.KeyChar != '-' && (e.KeyChar < '0' | e.KeyChar > '9')) e.Handled = true;
+			else if (box.Text.Length > 2 && e.KeyChar != '\b') e.Handled = true;
+			box.Validate(true);
 		}
 		public void Wage(object sender, EventArgs e)
 		{
-			if (numWager.Value < lastvalue)
-			{
-				numWager.Value += wagersave ;
-				//numWager.Increment -= wagerstep;
-
-			}
-			if (numWager.Value > lastvalue)
-			{
-				wagersave = wagerstep;
-				wagerstep = numWager.Increment;
-				numWager.Increment += wagersave;
-				Console.WriteLine($"wagersave: {wagersave}");
-				Console.WriteLine($"wagerstep: {wagerstep}");
-				Console.WriteLine($"numWager.Increment: {numWager.Increment}");
-				Console.WriteLine($"numWager.Value: {numWager.Value}");
-			}
-			lastvalue = numWager.Value;
 		}
 	}
 }
