ed6e3b73a504fce9234c0cf0ece80d65ff7c188c
Carson Mathre <carsonmathre@gmail.com>
2022-04-08 14:55:49 -0500

    Begin work on My Friends program

---
+++ Forms/frm23Friends.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Mathre
+{
+	partial class Frm23Friends
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.pnlFrame = new System.Windows.Forms.Panel();
+            this.pnlBody2 = new System.Windows.Forms.Panel();
+            this.pnlBody = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.pnlFrame.SuspendLayout();
+            this.pnlBody.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlFrame
+            // 
+            this.pnlFrame.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlFrame.BackColor = System.Drawing.Color.Transparent;
+            this.pnlFrame.Controls.Add(this.pnlBody2);
+            this.pnlFrame.Controls.Add(this.pnlBody);
+            this.pnlFrame.Controls.Add(this.lblTitle);
+            this.pnlFrame.Location = new System.Drawing.Point(-8, -20);
+            this.pnlFrame.Name = "pnlFrame";
+            this.pnlFrame.Size = new System.Drawing.Size(1000, 500);
+            this.pnlFrame.TabIndex = 34;
+            // 
+            // pnlBody2
+            // 
+            this.pnlBody2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlBody2.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBody2.Location = new System.Drawing.Point(287, 276);
+            this.pnlBody2.Name = "pnlBody2";
+            this.pnlBody2.Size = new System.Drawing.Size(426, 136);
+            this.pnlBody2.TabIndex = 38;
+            // 
+            // pnlBody
+            // 
+            this.pnlBody.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlBody.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlBody.Controls.Add(this.txtSearch);
+            this.pnlBody.Controls.Add(this.button2);
+            this.pnlBody.Controls.Add(this.button1);
+            this.pnlBody.Controls.Add(this.btnInput);
+            this.pnlBody.Location = new System.Drawing.Point(287, 165);
+            this.pnlBody.Name = "pnlBody";
+            this.pnlBody.Size = new System.Drawing.Size(426, 105);
+            this.pnlBody.TabIndex = 32;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(187, 95);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(626, 51);
+            this.lblTitle.TabIndex = 31;
+            this.lblTitle.Text = "My Friends";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(171, 41);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(85, 23);
+            this.btnInput.TabIndex = 1;
+            this.btnInput.Text = "&Input Contacts";
+            this.btnInput.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Search";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(285, 81);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // Frm23Friends
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.pnlFrame);
+            this.Name = "Frm23Friends";
+            this.Text = "frm23Friends";
+            this.pnlFrame.ResumeLayout(false);
+            this.pnlBody.ResumeLayout(false);
+            this.pnlBody.PerformLayout();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		public System.Windows.Forms.Panel pnlFrame;
+		private System.Windows.Forms.Panel pnlBody2;
+		private System.Windows.Forms.Panel pnlBody;
+		internal System.Windows.Forms.Label lblTitle;
+		private System.Windows.Forms.Button btnInput;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox txtSearch;
+	}
+}
---
+++ Forms/frm23Friends.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Mathre
+{
+	public partial class Frm23Friends : Form, Forms.IManager
+	{
+		string[] friends = new string[5];
+		public Frm23Friends()
+		{
+			InitializeComponent();
+			btnInput.Click += Default;
+			txtSearch.TextChanged += Default;
+		}
+		public void Default(object sender, EventArgs e)
+		{
+			if (ReferenceEquals(btnInput, sender))
+			{
+				for (int i = 0; i < friends.Length; i++)
+				{
+					friends[i] = Microsoft.VisualBasic.Interaction.InputBox($"Input Name of Contact {i+1}");
+				}
+			}
+			//else if (ReferenceEquals(btnSort, sender))
+			//{
+
+			//}
+			else if (ReferenceEquals(txtSearch, sender))
+			{
+				foreach (string name in friends)
+				{
+					if (!name.Contains(txtSearch.Text))
+					{
+						friends.GetValue(1);
+					}
+				}
+			}
+
+		}
+
+		public void InputFormatter(object sender, KeyPressEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
