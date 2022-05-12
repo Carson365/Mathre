7007029b66a63188336cebc0f2cc262e949ac2a3
Carson Mathre <carsonmathre@gmail.com>
2022-05-12 14:55:33 -0500

    Begin Turtle Run

---
+++ Forms/frm25aTurtle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+namespace Mathre
+{
+	public partial class Frm25aTurtle : Form
+	{
+		Frm24bInvaders F25b; //F25b
+		public Frm25aTurtle()
+		{
+			InitializeComponent();
+			F25b = new();
+			Shown += Default;
+			//VisibleChanged += (p, e) => F25b.tmrMain.Enabled = Visible;
+		}
+		public void Default(object sender, EventArgs e)
+		{
+			F25b.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			F25b.FormBorderStyle = FormBorderStyle.None;
+			F25b.Left = 2;
+			F25b.Top = 2;
+			F25b.TopLevel = false;
+			F25b.Visible = true;
+			F25b.Size = new System.Drawing.Size(pnlBody.Width - 4, pnlBody.Height - 4);
+			pnlBody.Controls.Add(F25b);
+		}
+	}
+}
