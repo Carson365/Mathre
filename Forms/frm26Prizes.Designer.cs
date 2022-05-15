2620fe32ea04a52122fd762783aa8a0bae9baf7c
Carson Mathre <carsonmathre@gmail.com>
2022-05-15 02:27:59 -0500

    Final Touches on Hidden Prizes

--- Forms/frm26Prizes.Designer.cs
+++ Forms/frm26Prizes.Designer.cs
@@ -31,27 +31,20 @@
             this.pnlFrame = new System.Windows.Forms.Panel();
             this.pnlBody = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.pnlBody3 = new System.Windows.Forms.Panel();
-            this.lblPauseKey = new System.Windows.Forms.Label();
-            this.lblPause = new System.Windows.Forms.Label();
-            this.lblLeftKey = new System.Windows.Forms.Label();
-            this.lblRightKey = new System.Windows.Forms.Label();
-            this.lblShootKey = new System.Windows.Forms.Label();
-            this.lblShoot = new System.Windows.Forms.Label();
-            this.lblRight = new System.Windows.Forms.Label();
-            this.lblLeft = new System.Windows.Forms.Label();
-            this.lblControls = new System.Windows.Forms.Label();
             this.pnlBody2 = new System.Windows.Forms.Panel();
             this.lblY = new System.Windows.Forms.Label();
             this.lblX = new System.Windows.Forms.Label();
             this.nudY = new System.Windows.Forms.NumericUpDown();
             this.nudX = new System.Windows.Forms.NumericUpDown();
             this.btnPlay = new System.Windows.Forms.Button();
+            this.pnlBody3 = new System.Windows.Forms.Panel();
+            this.lblConstruction = new System.Windows.Forms.Label();
+            this.lblScoreboard = new System.Windows.Forms.Label();
             this.pnlFrame.SuspendLayout();
-            this.pnlBody3.SuspendLayout();
             this.pnlBody2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudY)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudX)).BeginInit();
+            this.pnlBody3.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlFrame
@@ -60,8 +53,8 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlFrame.BackColor = System.Drawing.Color.Transparent;
-            this.pnlFrame.Controls.Add(this.pnlBody2);
             this.pnlFrame.Controls.Add(this.pnlBody3);
+            this.pnlFrame.Controls.Add(this.pnlBody2);
             this.pnlFrame.Controls.Add(this.pnlBody);
             this.pnlFrame.Controls.Add(this.lblTitle);
             this.pnlFrame.Location = new System.Drawing.Point(-8, -20);
@@ -89,106 +82,6 @@
             this.lblTitle.TabIndex = 31;
             this.lblTitle.Text = "Hidden Prizes";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // pnlBody3
-            // 
-            this.pnlBody3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pnlBody3.BackColor = System.Drawing.SystemColors.Control;
-            this.pnlBody3.Controls.Add(this.lblPauseKey);
-            this.pnlBody3.Controls.Add(this.lblPause);
-            this.pnlBody3.Controls.Add(this.lblLeftKey);
-            this.pnlBody3.Controls.Add(this.lblRightKey);
-            this.pnlBody3.Controls.Add(this.lblShootKey);
-            this.pnlBody3.Controls.Add(this.lblShoot);
-            this.pnlBody3.Controls.Add(this.lblRight);
-            this.pnlBody3.Controls.Add(this.lblLeft);
-            this.pnlBody3.Controls.Add(this.lblControls);
-            this.pnlBody3.Location = new System.Drawing.Point(209, 150);
-            this.pnlBody3.Name = "pnlBody3";
-            this.pnlBody3.Size = new System.Drawing.Size(155, 260);
-            this.pnlBody3.TabIndex = 40;
-            // 
-            // lblPauseKey
-            // 
-            this.lblPauseKey.AutoSize = true;
-            this.lblPauseKey.Location = new System.Drawing.Point(86, 197);
-            this.lblPauseKey.Name = "lblPauseKey";
-            this.lblPauseKey.Size = new System.Drawing.Size(43, 13);
-            this.lblPauseKey.TabIndex = 8;
-            this.lblPauseKey.Text = "Escape";
-            // 
-            // lblPause
-            // 
-            this.lblPause.AutoSize = true;
-            this.lblPause.Location = new System.Drawing.Point(26, 197);
-            this.lblPause.Name = "lblPause";
-            this.lblPause.Size = new System.Drawing.Size(37, 13);
-            this.lblPause.TabIndex = 7;
-            this.lblPause.Text = "Pause";
-            // 
-            // lblLeftKey
-            // 
-            this.lblLeftKey.AutoSize = true;
-            this.lblLeftKey.Location = new System.Drawing.Point(115, 101);
-            this.lblLeftKey.Name = "lblLeftKey";
-            this.lblLeftKey.Size = new System.Drawing.Size(14, 13);
-            this.lblLeftKey.TabIndex = 6;
-            this.lblLeftKey.Text = "A";
-            // 
-            // lblRightKey
-            // 
-            this.lblRightKey.AutoSize = true;
-            this.lblRightKey.Location = new System.Drawing.Point(114, 133);
-            this.lblRightKey.Name = "lblRightKey";
-            this.lblRightKey.Size = new System.Drawing.Size(15, 13);
-            this.lblRightKey.TabIndex = 5;
-            this.lblRightKey.Text = "D";
-            // 
-            // lblShootKey
-            // 
-            this.lblShootKey.AutoSize = true;
-            this.lblShootKey.Location = new System.Drawing.Point(91, 165);
-            this.lblShootKey.Name = "lblShootKey";
-            this.lblShootKey.Size = new System.Drawing.Size(38, 13);
-            this.lblShootKey.TabIndex = 4;
-            this.lblShootKey.Text = "Space";
-            // 
-            // lblShoot
-            // 
-            this.lblShoot.AutoSize = true;
-            this.lblShoot.Location = new System.Drawing.Point(26, 165);
-            this.lblShoot.Name = "lblShoot";
-            this.lblShoot.Size = new System.Drawing.Size(35, 13);
-            this.lblShoot.TabIndex = 3;
-            this.lblShoot.Text = "Shoot";
-            // 
-            // lblRight
-            // 
-            this.lblRight.AutoSize = true;
-            this.lblRight.Location = new System.Drawing.Point(26, 133);
-            this.lblRight.Name = "lblRight";
-            this.lblRight.Size = new System.Drawing.Size(32, 13);
-            this.lblRight.TabIndex = 2;
-            this.lblRight.Text = "Right";
-            // 
-            // lblLeft
-            // 
-            this.lblLeft.AutoSize = true;
-            this.lblLeft.Location = new System.Drawing.Point(26, 101);
-            this.lblLeft.Name = "lblLeft";
-            this.lblLeft.Size = new System.Drawing.Size(25, 13);
-            this.lblLeft.TabIndex = 1;
-            this.lblLeft.Text = "Left";
-            // 
-            // lblControls
-            // 
-            this.lblControls.AutoSize = true;
-            this.lblControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblControls.Location = new System.Drawing.Point(39, 39);
-            this.lblControls.Name = "lblControls";
-            this.lblControls.Size = new System.Drawing.Size(76, 20);
-            this.lblControls.TabIndex = 0;
-            this.lblControls.Text = "Controls";
             // 
             // pnlBody2
             // 
@@ -277,6 +170,36 @@
             this.btnPlay.Text = "Hide the Prizes!";
             this.btnPlay.UseVisualStyleBackColor = false;
             // 
+            // pnlBody3
+            // 
+            this.pnlBody3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlBody3.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlBody3.Controls.Add(this.lblConstruction);
+            this.pnlBody3.Controls.Add(this.lblScoreboard);
+            this.pnlBody3.Location = new System.Drawing.Point(209, 150);
+            this.pnlBody3.Name = "pnlBody3";
+            this.pnlBody3.Size = new System.Drawing.Size(155, 260);
+            this.pnlBody3.TabIndex = 35;
+            // 
+            // lblConstruction
+            // 
+            this.lblConstruction.AutoSize = true;
+            this.lblConstruction.Location = new System.Drawing.Point(28, 101);
+            this.lblConstruction.Name = "lblConstruction";
+            this.lblConstruction.Size = new System.Drawing.Size(98, 13);
+            this.lblConstruction.TabIndex = 1;
+            this.lblConstruction.Text = "Under Construction";
+            // 
+            // lblScoreboard
+            // 
+            this.lblScoreboard.AutoSize = true;
+            this.lblScoreboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreboard.Location = new System.Drawing.Point(39, 39);
+            this.lblScoreboard.Name = "lblScoreboard";
+            this.lblScoreboard.Size = new System.Drawing.Size(65, 20);
+            this.lblScoreboard.TabIndex = 0;
+            this.lblScoreboard.Text = "Scores";
+            // 
             // Frm26Prizes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -287,12 +210,12 @@
             this.Name = "Frm26Prizes";
             this.Text = "Hidden Prizes";
             this.pnlFrame.ResumeLayout(false);
-            this.pnlBody3.ResumeLayout(false);
-            this.pnlBody3.PerformLayout();
             this.pnlBody2.ResumeLayout(false);
             this.pnlBody2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudY)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudX)).EndInit();
+            this.pnlBody3.ResumeLayout(false);
+            this.pnlBody3.PerformLayout();
             this.ResumeLayout(false);
 
 		}
@@ -302,21 +225,14 @@
 		public System.Windows.Forms.Panel pnlFrame;
 		internal System.Windows.Forms.Label lblTitle;
 		private System.Windows.Forms.Panel pnlBody;
-		private System.Windows.Forms.Panel pnlBody3;
-		private System.Windows.Forms.Label lblPauseKey;
-		private System.Windows.Forms.Label lblPause;
-		private System.Windows.Forms.Label lblLeftKey;
-		private System.Windows.Forms.Label lblRightKey;
-		private System.Windows.Forms.Label lblShootKey;
-		private System.Windows.Forms.Label lblShoot;
-		private System.Windows.Forms.Label lblRight;
-		private System.Windows.Forms.Label lblLeft;
-		private System.Windows.Forms.Label lblControls;
 		private System.Windows.Forms.Panel pnlBody2;
 		private System.Windows.Forms.Label lblY;
 		private System.Windows.Forms.Button btnPlay;
 		private System.Windows.Forms.Label lblX;
 		private System.Windows.Forms.NumericUpDown nudY;
 		private System.Windows.Forms.NumericUpDown nudX;
+		private System.Windows.Forms.Panel pnlBody3;
+		private System.Windows.Forms.Label lblConstruction;
+		private System.Windows.Forms.Label lblScoreboard;
 	}
 }
--- Forms/frm26Prizes.cs
+++ Forms/frm26Prizes.cs
@@ -22,10 +22,11 @@ namespace Mathre
 		private void Setup()
 		{
 			Buttons = new Button[ButtonsX, ButtonsY];
-			for (int y = 0; y < Buttons.GetLength(1); y++)
+			for (int y = 0; y < Buttons.GetLength(1); y++) // Iterate for Y values
 			{
-				for (int x = 0; x < Buttons.GetLength(0); x++)
+				for (int x = 0; x < Buttons.GetLength(0); x++) // Iterate for X values
 				{
+					// Configure the button
 					Button b = new()
 					{
 						Size = new System.Drawing.Size(42, 42),
@@ -33,14 +34,15 @@ namespace Mathre
 					};
 					b.Click += Guess;
 					b.Enabled = true;
+					// Add it to the form and the tracking array
 					Contain.Controls.Add(b);
 					Buttons[x, y] = b;
 				}
 			}
+			// Set the container size and location, then add it to the form
 			Contain.Width = Buttons[ButtonsX - 1, ButtonsY - 1].Right + 11;
 			Contain.Height = Buttons[ButtonsX - 1, ButtonsY - 1].Bottom + 11;
 			Contain.Location = new System.Drawing.Point((pnlBody.Width - Contain.Width) / 2, (pnlBody.Height - Contain.Height) / 2);
-
 			pnlBody.Controls.Add(Contain);
 			Generate();
 		}
@@ -70,25 +72,25 @@ namespace Mathre
 		}
 		private void Guess(object sender, EventArgs e)
 		{
-			if (sender is Button b && b.Text == "")
+			if (sender is Button b && b.Text == "") // Ensure each button is only pressed once
 			{
 				if ($"{b.Tag}" == "1") { b.Text = "Comp"; b1 = true; b.BackColor = System.Drawing.Color.PaleGreen; }
 				else if ($"{b.Tag}" == "2") { b.Text = "uter"; b2 = true; b.BackColor = System.Drawing.Color.PaleGreen; }
 				else b.Text = "X";
 				guesses++;
-				if (guesses == 5 || b1 && b2)
+				if (guesses == 5 || b1 && b2) // end the game after 5 guesses or two correct
 				{
 					string un = (b1 && b2) switch { false => "un", _ => "" };
 					string winlose = (b1 && b2) switch { false => "Lose.", _ => "Win!" };
-					foreach (Button btn in Buttons)
+					foreach (Button btn in Buttons) // Show where the correct guesses were
 					{
 						btn.Enabled = false;
 						if ($"{btn.Tag}" == "1" && btn.Text == "") { btn.Text = "Comp"; btn.BackColor = System.Drawing.Color.LightCoral; }
 						else if ($"{btn.Tag}" == "2" && btn.Text == "") { btn.Text = "uter"; btn.BackColor = System.Drawing.Color.LightCoral; }
-						guesses = 0;
-						b1 = false;
-						b2 = false;
 					}
+					guesses = 0;
+					b1 = false;
+					b2 = false;
 					MessageBox.Show($"You {winlose}\nYou were {un}able to find both word fragments.");
 					Play(null, null); // Comment this line out to restart the game manually
 				}
